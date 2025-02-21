a76f475e4b900f4896f60ffebe48bf8210a6fb36
leao <lucasleaolima@gmail.com>
2025-02-21 15:34:48 -0300

    mod test

--- order/models/order.py
+++ order/models/order.py
@@ -5,3 +5,4 @@ from ...product.models.product import Product
 class Order(models.Model):
     product = models.ManyToManyField(Product, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+
