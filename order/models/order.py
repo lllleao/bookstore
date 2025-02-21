a257886a0b213fd8abb0677ebf797274cfd8f488
leao <lucasleaolima@gmail.com>
2025-02-21 15:30:55 -0300

    configuration models

---
+++ order/models/order.py
@@ -0,0 +1,7 @@
+from django.db import models # type: ignore
+from django.contrib.auth.models import User # type: ignore
+from ...product.models.product import Product
+
+class Order(models.Model):
+    product = models.ManyToManyField(Product, blank=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
