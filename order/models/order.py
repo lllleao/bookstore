e8b223c6f7dd3ec5f30a5b714b67cdc5060032e1
leao <lucasleaolima@gmail.com>
2025-02-21 15:39:57 -0300

    redelete

--- order/models/order.py
+++ order/models/order.py
@@ -5,5 +5,3 @@ from ...product.models.product import Product
 class Order(models.Model):
     product = models.ManyToManyField(Product, blank=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-
-
