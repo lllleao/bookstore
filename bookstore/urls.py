59c6de2547d3458e8155eb61689401587eaf5cdf
lucas leao <lucasleaolima@gmail.com>
2025-04-27 18:35:12 -0300

    automatic upload

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -18,6 +18,7 @@ import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
 from rest_framework.authtoken.views import obtain_auth_token
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,4 +26,6 @@ urlpatterns = [
     re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
     re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls')),
     path("api-token-auth/", obtain_auth_token, name='api_token_auth'),
+    path("update_server/", views.update, name="update"),
+    path("hello/", views.hello_world, name="hello_world"),
 ]
